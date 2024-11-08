2f0506e4e1ad00143bba6320d80b2285232ac518
vkunguyenthanhcong <ntcong.20it5@vku.udn.vn>
2024-11-08 15:06:18 +0700

    update login

--- src/services/PublicRoute.js
+++ src/services/PublicRoute.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
+import {jwtDecode} from "jwt-decode";
 
+const isTokenExpired = (token) => {
+    if (!token) return true;
+
+    try {
+        const decoded = jwtDecode(token);
+        const currentTime = Date.now() / 1000; // Current time in seconds
+        return decoded.exp < currentTime; // Check if token is expired
+    } catch (error) {
+        console.error('Token decoding failed:', error);
+        return true; // If decoding fails, consider the token expired
+    }
+};
 const checkAuth = () => {
     const token = localStorage.getItem('token');
     if(token != null){
-        return true;
+        if(isTokenExpired(token)){
+            return false;
+        }else {
+            return true;
+        }
     }else{
         return false;
     }
